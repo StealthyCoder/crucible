200b7b3f324c8c25cb19969892464fddab04b027
Eric Bode <eric.bode@globalorange.nl>
2019-10-11 01:24:50 +0200

    Fixed some naming issues

--- src/core/mould.sh
+++ src/core/mould.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function require {
-    local local_crucible_version shared_crucible_version shared_location
+    local local_crucible_version shared_crucible_version shared_location module_name
     local_crucible_version="$(grep 'version' .crucible | cut -d'=' -f 2)"
     shared_crucible_version="$( grep 'version' "$(grep 'shared_dir' .crucible | cut -d'=' -f 2)"/.crucible | cut -d'=' -f 2)"
     if [ "$local_crucible_version" != "$shared_crucible_version" ]
@@ -15,16 +15,17 @@ function require {
         exit 1
     fi
     shared_location="$(grep 'shared_dir' .crucible | cut -d'=' -f 2)/"
-    if [ -f "$shared_location$1" ]
+    module_name="$(echo $1 | cut -d'/' -f 2)"
+    if [ -f "$shared_location$module_name" ]
     then
         local module_version
-        module_version="$(grep 'CRUCIBLE_VERSION' "$shared_location$1" | cut -d'=' -f 2 )"
+        module_version="$(grep 'CRUCIBLE_VERSION' "$shared_location$module_name" | cut -d'=' -f 2 )"
         if [ "$module_version" != "$local_crucible_version" ]
         then 
-            wget -q -O "$shared_location$1" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
+            wget -q -O "$shared_location$module_name" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
         fi
     else
-        wget -q -O "$shared_location$1" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
+        wget -q -O "$shared_location$module_name" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
     fi
-    source "$shared_location$1"
+    source "$shared_location$module_name"
 }
