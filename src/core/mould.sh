f33c0cae11d1e6739732f912f884f50b99af7390
Eric Bode <eric.bode@globalorange.nl>
2019-10-11 01:16:20 +0200

    Some typo fixes

--- src/core/mould.sh
+++ src/core/mould.sh
@@ -2,28 +2,28 @@
 
 function require {
     local local_crucible_version shared_crucible_version shared_location
-	local_crucible_version="$(grep 'version' .crucible | cut -d'=' -f 2)"
-	shared_crucible_version="$( grep 'version' "$(grep 'shared_dir' .crucible | cut -d'=' -f 2)"/.crucible | cut -d'=' -f 2)"
-	if [ "$local_crucible_version" != "$shared_crucible_version" ]
+    local_crucible_version="$(grep 'version' .crucible | cut -d'=' -f 2)"
+    shared_crucible_version="$( grep 'version' "$(grep 'shared_dir' .crucible | cut -d'=' -f 2)"/.crucible | cut -d'=' -f 2)"
+    if [ "$local_crucible_version" != "$shared_crucible_version" ]
     then 
         echo "Versions out of sync. Please run crucible update."
         exit 1 
     fi
-	if [ -z "$1" ]
+    if [ -z "$1" ]
     then 
         echo "Module to require has to be specified."
         exit 1
     fi
-	shared_location="$(grep 'shared_dir' .crucible | cut -d'=' -f 2)/"
-	if [ -f "$shared_location$1" ]
+    shared_location="$(grep 'shared_dir' .crucible | cut -d'=' -f 2)/"
+    if [ -f "$shared_location$1" ]
     then
-		local module_version
-		module_version="$(grep 'CRUCIBLE_VERSION' "$shared_location$1" | cut -d'=' -f 2 )"
-		if [ "$module_version" != "$local_crucible_version" ]
+        local module_version
+        module_version="$(grep 'CRUCIBLE_VERSION' "$shared_location$1" | cut -d'=' -f 2 )"
+        if [ "$module_version" != "$local_crucible_version" ]
         then 
             wget -q -O "$shared_location$1" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
         fi
-	else
-		wget -q -O "$shared_location$1" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
+    else
+        wget -q -O "$shared_location$1" "https://raw.githubusercontent.com/StealthyCoder/crucible/mould/src/${1%.sh}.sh"
     fi
 }
