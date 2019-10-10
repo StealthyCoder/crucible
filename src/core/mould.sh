486974cad9d2edb144e7534c2bab1c13fb61172a
Eric Bode <eric.bode@globalorange.nl>
2019-10-11 01:41:27 +0200

    Added option to shellcheck and meta data to mould template

--- src/core/mould.sh
+++ src/core/mould.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+### CRUCIBLE META DATA ###
+# CRUCIBLE_VERSION=0.1.0
+# CRUCIBLE_AUTHOR=StealthyCoder
+# CRUCIBLE_CREATED=1570750276
+### CRUCIBLE META DATA ###
 
 function require {
     local local_crucible_version shared_crucible_version shared_location module_name
