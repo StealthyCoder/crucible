031ef57a82471479ed9490b8305a09be65dea95d
Eric Bode <eric.bode@globalorange.nl>
2020-01-03 12:45:12 +0100

    Added optional params to be used for testing

--- src/bash/dates/local.sh
+++ src/bash/dates/local.sh
@@ -6,45 +6,111 @@
 ### CRUCIBLE META DATA ###
 
 function dates.local.now {
-    date
+    if [ -z "$1" ]
+    then
+        date
+    else
+        date "$1"
+    fi
+    
 }
 
 function dates.local.now.timestamp {
-    date +%s
+    if [ -z "$1" ]
+    then
+        date +%s
+    else
+        date +%s "$1"
+    fi
+    
 }
 
 function dates.local.rfc3339.ns {
-    date --rfc-3339=ns
+    if [ -z "$1" ]
+    then
+        date --rfc-3339=ns
+    else
+        date --rfc-3339=ns "$1"
+    fi
+    
 }
 
 function dates.local.rfc3339.seconds {
-    date --rfc-3339=seconds
+    if [ -z "$1" ]
+    then
+        date --rfc-3339=seconds
+    else
+        date --rfc-3339=seconds "$1"
+    fi
+    
 }
 
 function dates.local.rfc3339.date {
-    date --rfc-3339=date
+    if [ -z "$1" ]
+    then
+        date --rfc-3339=date
+    else
+        date --rfc-3339=date "$1"
+    fi
+    
 }
 
 function dates.local.email {
-    date --rfc-email
+    if [ -z "$1" ]
+    then
+        date --rfc-email
+    else
+        date --rfc-email "$1"
+    fi
+    
 }
 
 function dates.local.iso8601.ns {
-    date --iso-8601=ns
+    if [ -z "$1" ]
+    then
+        date --iso-8601=ns
+    else
+        date --iso-8601=ns "$1"
+    fi
+    
 }
 
 function dates.local.is8601.seconds {
-    date --iso-8601=seconds
+    if [ -z "$1" ]
+    then
+        date --iso-8601=seconds
+    else
+        date --iso-8601=seconds "$1"
+    fi
+    
 }
 
 function dates.local.iso8601.minutes {
-    date --iso-8601=minutes
+    if [ -z "$1" ]
+    then
+        date --iso-8601=minutes
+    else
+        date --iso-8601=minutes "$1"
+    fi
+    
 }
 
 function dates.local.iso8601.hours {
-    date --iso-8601=hours
+    if [ -z "$1" ]
+    then
+        date --iso-8601=hours
+    else
+        date --iso-8601=hours "$1"
+    fi
+    
 }
 
 function dates.local.iso8601.date {
-    date --iso-8601=date
+    if [ -z "$1" ]
+    then
+        date --iso-8601=date
+    else
+        date --iso-8601=date "$1"
+    fi
+    
 }
--- src/bash/dates/utc.sh
+++ src/bash/dates/utc.sh
@@ -6,45 +6,101 @@
 ### CRUCIBLE META DATA ###
 
 function dates.utc.now {
-    date -u
+    if [ -z "$1" ]
+    then
+        date -u
+    else
+        date -u "$1"
+    fi
+    
 }
 
 function dates.utc.now.timestamp {
-    date -u +%s
+    if [ -z "$1" ]
+    then
+        date -u +%s
+    else
+        date -u +%s "$1"
+    fi
 }
 
 function dates.utc.rfc3339.ns {
-    date -u --rfc-3339=ns
+    if [ -z "$1" ]
+    then
+        date -u --rfc-3339=ns
+    else
+        date -u --rfc-3339=ns "$1"
+    fi
 }
 
 function dates.utc.rfc3339.seconds {
-    date -u --rfc-3339=seconds
+    if [ -z "$1" ]
+    then
+        date -u --rfc-3339=seconds
+    else
+        date -u --rfc-3339=seconds "$1"
+    fi
 }
 
 function dates.utc.rfc3339.date {
-    date -u --rfc-3339=date
+    if [ -z "$1" ]
+    then
+        date -u --rfc-3339=date
+    else
+        date -u --rfc-3339=date "$1"
+    fi    
 }
 
 function dates.utc.email {
-    date -u --rfc-email
+    if [ -z "$1" ]
+    then
+        date -u --rfc-email    
+    else
+        date -u --rfc-email "$1"
+    fi
 }
 
 function dates.utc.iso8601.ns {
-    date -u --iso-8601=ns
+    if [ -z "$1" ]
+    then
+        date -u --iso-8601=ns
+    else
+        date -u --iso-8601=ns "$1"
+    fi
 }
 
 function dates.utc.is8601.seconds {
-    date -u --iso-8601=seconds
+    if [ -z "$1" ]
+    then
+        date -u --iso-8601=seconds
+    else
+        date -u --iso-8601=seconds "$1"
+    fi
 }
 
 function dates.utc.iso8601.minutes {
-    date -u --iso-8601=minutes
+    if [ -z "$1" ]
+    then
+        date -u --iso-8601=minutes
+    else
+        date -u --iso-8601=minutes "$1"
+    fi
 }
 
 function dates.utc.iso8601.hours {
-    date -u --iso-8601=hours
+    if [ -z "$1" ]
+    then
+        date -u --iso-8601=hours
+    else
+        date -u --iso-8601=hours "$1"
+    fi
 }
 
 function dates.utc.iso8601.date {
-    date -u --iso-8601=date
-}
+    if [ -z "$1" ]
+    then
+        date -u --iso-8601=date
+    else
+        date -u --iso-8601=date "$1"
+    fi
+} 
