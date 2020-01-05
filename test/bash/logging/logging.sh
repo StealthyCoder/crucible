53839eae1d39f1051c6d573b8dfcb3bb88d5e66c
Eric Bode <eric.bode@globalorange.nl>
2020-01-05 10:49:56 +0100

    Fixed logging tests

--- test/bash/logging/logging.sh
+++ test/bash/logging/logging.sh
@@ -51,7 +51,7 @@ function logging.logging.info {
     intro "logging.logging.info"
     local msg
     msg="$(logging.info Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[38;5;86m[ INFO ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[38\;5\;86m\[\ INFO\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
 }
 
@@ -63,7 +63,7 @@ function logging.logging.debug {
 
     logging.set_level "$(logging.level.debug)"
     msg="$(logging.debug Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[34m[ DEBUG ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[34m\[\ DEBUG\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
     unset CRUCIBLE_LOGGING_LEVEL
 }
@@ -72,7 +72,7 @@ function logging.logging.error {
     intro "logging.logging.error"
     local msg
     msg="$(logging.error Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[38;5;196m[ ERROR ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[38\;5\;196m\[\ ERROR\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
 }
 
@@ -84,7 +84,7 @@ function logging.logging.warning {
 
     logging.set_level "$(logging.level.warning)"
     msg="$(logging.warning Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[93m[ WARNING ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[93m\[\ WARNING\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
     unset CRUCIBLE_LOGGING_LEVEL
 }
@@ -93,7 +93,7 @@ function logging.logging.success {
     intro "logging.logging.success"
     local msg
     msg="$(logging.success Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[38;5;82m[ SUCCESS ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[38\;5\;82m\[\ SUCCESS\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
 }
 
@@ -101,7 +101,7 @@ function logging.logging.message {
     intro "logging.logging.message"
     local msg
     msg="$(logging.message Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[38;5;86m[ INFO ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[38\;5\;86m\[\ INFO\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
 }
 
@@ -109,7 +109,7 @@ function logging.logging.log {
     intro "logging.logging.log"
     local msg
     msg="$(logging.log Cookie | tr -d -C '[:print:]')"
-    test "$msg" = "[38;5;86m[ INFO ] Cookie [0m" || fail "Message was wrong format $msg"
+    [[ "$msg" =~ \[38\;5\;86m\[\ INFO\ \].*Cookie ]] || fail "Message was wrong format $msg"
     success
 }
 
