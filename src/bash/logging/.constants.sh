a1d56d11af7f0c668bfa42826c1f7d07674680c2
Eric Bode <eric.bode@globalorange.nl>
2020-02-21 13:00:57 +0100

    Bumped version to 0.2.2

--- src/bash/logging/.constants.sh
+++ src/bash/logging/.constants.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 ### CRUCIBLE META DATA ###
-# CRUCIBLE_VERSION=0.2.3
+# CRUCIBLE_VERSION=0.2.2
 # CRUCIBLE_AUTHOR=StealthyCoder
-# CRUCIBLE_CREATED=1584529915
+# CRUCIBLE_CREATED=1582286436
 ### CRUCIBLE META DATA ###
 
 function logging.color.escape_char {
