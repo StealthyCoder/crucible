fd899e99a8b17e0fd4205cbcfe741abb8c4edb62
Eric Bode <eric.bode@globalorange.nl>
2020-07-27 18:24:05 +0200

    Trying to fix a test

--- test/bash/arrays/arrays.sh
+++ test/bash/arrays/arrays.sh
@@ -159,7 +159,8 @@ function arrays.arrays.entries {
     test $? -eq 0 || fail "Could not get from array"
     
     b="$(echo "$b" | tr -d "[:cntrl:]")"
-    test "$b" = "Index: 0, Value: 1Index: 1, Value: 2" || fail "Did not get correct element"
+    echo "$b"
+    test "$b" = "0,11,2" || fail "Did not get correct element"
 
     success
 }
