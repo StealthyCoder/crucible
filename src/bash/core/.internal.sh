50957734465b2feceafc6fafa27ea82d2ac6df4c
Eric Bode <eric.bode@globalorange.nl>
2020-07-27 17:01:11 +0200

    Restructured io/text plus local array check

--- src/bash/core/.internal.sh
+++ src/bash/core/.internal.sh
@@ -28,7 +28,7 @@ function __verify_nr_args {
 function __verify_if_arg_is_array {
     if [[ ! "$(declare -p "$1" 2>/dev/null)" =~ ^declare\ -ax\ "$1"= ]]
     then
-        if [[ ! "$(declare -p "$1" 2>/dev/null)" =~ ^declare\ -a\ "$1"= ]]
+        if [[ ! "$(declare -p "$1" 2>/dev/null)" =~ ^declare\ -a\ "$1" ]]
         then
             logging.error "Argument needs to be an array created by arrays.transform_into_array"
             logging.error "$(declare -p "$1")"
