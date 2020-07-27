eb686931f2b40b25a372064edc6c242659e8875e
Eric Bode <eric.bode@globalorange.nl>
2020-07-27 15:35:50 +0200

    Added start of text I/O

---
+++ src/bash/io/text.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+### CRUCIBLE META DATA ###
+# CRUCIBLE_VERSION={CRUCIBLE_VERSION}
+# CRUCIBLE_AUTHOR=StealthyCoder
+# CRUCIBLE_CREATED={CRUCIBLE_CREATED}
+### CRUCIBLE META DATA ###
+
+require core/.internal
+
+function text.split_by_char {
+    __verify_nr_args "$#" 2 text.split_by_char
+    echo "$1 | sed -e 's/$2/ /g'"
+}
