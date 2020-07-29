f04700ebb8d304b4a757995224fde1e8155c88f1
Eric Bode <eric.bode@globalorange.nl>
2020-07-29 15:45:26 +0200

    Added contains, values, keys, entries

--- src/bash/maps/maps.sh
+++ src/bash/maps/maps.sh
@@ -16,7 +16,7 @@ function maps.transform_into_map {
     eval "$print"
 }
 
-function maps.add {
+function maps.put {
     __verify_nr_args "$#" 3 maps.add
     
     if __verify_if_arg_is_map "$1"
@@ -38,10 +38,10 @@ function maps.add {
 }
 
 function maps.push {
-    maps.add "$1" "$2" "$3"
+    maps.put "$1" "$2" "$3"
 }
 
-function maps.add_all {
+function maps.put_all {
     if __verify_if_arg_is_map "$1"
     then
         if [ "$(( $# % 2 ))" -eq 0 ]
@@ -50,7 +50,7 @@ function maps.add_all {
             exit 1
         fi
         local arr key value print export_name
-        if __check_if_arg_is_local_array "$1"
+        if __check_if_arg_is_local_map "$1"
         then
             print="$(declare -p "$1" | sed -e "s/declare -A $1=/declare -A arr=/" )"
         else
@@ -69,5 +69,142 @@ function maps.add_all {
         done        
         print="$(declare -p arr | sed -e "s/declare -A arr=/declare -Agx $export_name=/" )"
         eval "$print"
+    fi
+}
+
+function maps.merge {
+    if __verify_if_arg_is_map "$1"
+    then
+        if __verify_if_arg_is_map "$2"
+        then
+            local src target print
+            print="$(declare -p "$1" | sed -e "s/declare -Ax $1=/declare -A src=/" )"
+            eval "$print"
+            print="$(declare -p "$2" | sed -e "s/declare -Ax $2=/declare -A target=/" )"
+            eval "$print"
+            for key in "${!target[@]}"
+            do
+                src+=(["$key"]="${target[$key]}")
+            done
+            print="$(declare -p src | sed -e "s/declare -A src=/declare -Agx $1=/" )"
+            eval "$print"
+        fi
+    fi
+}
+
+function maps.get {
+    __verify_nr_args "$#" 2 maps.get
+    if __verify_if_arg_is_map "$1"
+    then
+        local arr print
+        if __check_if_arg_is_local_map "$1"
+        then
+            print="$(declare -p "$1" | sed -e "s/declare -A $1=/declare -A arr=/" )"
+        else
+            print="$(declare -p "$1" | sed -e "s/declare -Ax $1=/declare -A arr=/" )"
+        fi
+        eval "$print"
+        echo "${arr[$2]}"
+    fi
+}
+
+function maps.remove {
+    __verify_nr_args "$#" 2 maps.remove
+    if __verify_if_arg_is_map "$1"
+    then
+        local arr print
+        if __check_if_arg_is_local_map "$1"
+        then
+            print="$(declare -p "$1" | sed -e "s/declare -A $1=/declare -A arr=/" )"
+        else
+            print="$(declare -p "$1" | sed -e "s/declare -Ax $1=/declare -A arr=/" )"
+        fi
+        eval "$print"
+        unset arr["$2"]
+        print="$(declare -p arr | sed -e "s/declare -A arr=/declare -Agx $1=/" )"
+        declare -p arr
+        eval "$print"
+    fi
+}
+
+function maps.values {
+    if __verify_if_arg_is_map "$1"
+    then
+
+        local arr print
+        
+        if __check_if_arg_is_local_map "$1"
+        then
+            print="$(declare -p "$1" | sed -e "s/declare -A $1=/declare -A arr=/" )"
+        else
+            print="$(declare -p "$1" | sed -e "s/declare -Ax $1=/declare -A arr=/" )"
+        fi
+        eval "$print"
+        echo "${arr[@]}"
+    fi
+}
+
+function maps.keys {
+    if __verify_if_arg_is_map "$1"
+    then
+
+        local arr print
+        
+        if __check_if_arg_is_local_map "$1"
+        then
+            print="$(declare -p "$1" | sed -e "s/declare -A $1=/declare -A arr=/" )"
+        else
+            print="$(declare -p "$1" | sed -e "s/declare -Ax $1=/declare -A arr=/" )"
+        fi
+        eval "$print"
+        echo "${!arr[@]}"
+    fi
+}
+
+function maps.entries {
+    if __verify_if_arg_is_map "$1"
+    then
+
+        local arr print
+        
+        if __check_if_arg_is_local_map "$1"
+        then
+            print="$(declare -p "$1" | sed -e "s/declare -A $1=/declare -A arr=/" )"
+        else
+            print="$(declare -p "$1" | sed -e "s/declare -Ax $1=/declare -A arr=/" )"
+        fi
+        eval "$print"
+        for key in "${!arr[@]}"
+        do
+            echo "$key,${arr[$key]}"
+        done
+    fi
+}
+
+function maps.contains_key {
+    __verify_nr_args "$#" 2 maps.contains_key
+    if __verify_if_arg_is_map "$1"
+    then
+        local keys
+        keys="$(maps.keys "$1")"
+        if [ "$(echo "$keys" | grep -c "$2" )" -ge 1 ]
+        then
+            return 0 
+        fi
+        return 1
+    fi
+}
+
+function maps.contains_value {
+    __verify_nr_args "$#" 2 maps.contains_value
+    if __verify_if_arg_is_map "$1"
+    then
+        local values
+        values="$(maps.values "$1")"
+        if [ "$(echo "$values" | grep -c "$2" )" -ge 1 ]
+        then
+            return 0 
+        fi
+        return 1
     fi
 }
