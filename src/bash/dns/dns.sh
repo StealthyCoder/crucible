1052e693ae05aa5ce8927322cd5abe59a920f304
Eric Bode <eric.bode@foundries.io>
2023-10-09 15:10:07 +0200

    fix(dns): Forgot DNS ID to Map
    Signed-off-by: Eric Bode <eric.bode@foundries.io>

--- src/bash/dns/dns.sh
+++ src/bash/dns/dns.sh
@@ -12,9 +12,10 @@ function dns.poc {
     network.udp 8.8.8.8 53
     # RECORD symbolizing QUERY for A for example.com
     # https://routley.io/posts/hand-writing-dns-messages
+    # NOTE: ID = AAAA in decimal is 43690
     printf "\xAA\xAA\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x07\x65\x78\x61\x6d\x70\x6c\x65\x03\x63\x6f\x6d\x00\x00\x01\x00\x01" >&"$CRUCIBLE_NETWORK_FD"
     network.udp.receive "$CRUCIBLE_NETWORK_FD"
-    maps.entries DNS_RESPONSE_"$id"
+    maps.entries DNS_RESPONSE_43690
     logging.info "Things left:"
     logging.info "1. Write different query types (A, AAAA, TXT...)"
     logging.info "2. Parse input into request"
